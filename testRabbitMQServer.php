05c9aac718f6fce4bc5d7f43a82fa36e284bd0c1
Rea <aksihombing05@gmail.com>
2025-09-29 13:54:59 -0400

    Edited MQServer and Client

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -4,14 +4,42 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+
+// LOGIN FUNCTION
 function doLogin($username,$password)
 {
     // lookup username in databas
     // check password
-    return true;
+    $login = new loginDB(); // what is loginDB?
+    return $login->validateLogin($username,$password);
+    //return true;
     //return false if not valid
 }
 
+// REGISTER FUNCTION
+function doRegister($email,$username,$password)
+{
+  $login = new loginDB(); // what db do we need?
+  return $login->validateLogin($username,$password);
+}
+
+
+
+
+// VALIDATE SESSION FUNCTION
+function doValidate($_SESSION) // idk how to fix this ??
+{
+  if (!isset($_SESSION['sessionId'])){
+    echo "Invalid session";
+  }
+  else{
+    echo "Session is valid";
+  }
+}
+
+
+
+// ------------- PROCESS DATA ------------
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
@@ -22,13 +50,20 @@ function requestProcessor($request)
   }
   switch ($request['type'])
   {
+    case "register":
+      return doRegister($request['email'],$request['username'],$request['password']);
     case "login":
       return doLogin($request['username'],$request['password']);
     case "validate_session":
       return doValidate($request['sessionId']);
+      
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
+
+
+
+
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
