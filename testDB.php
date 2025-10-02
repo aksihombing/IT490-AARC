5a4b64d094099ec24b646f6d73a4accbc18e11e8
Chizi <157385976+Cae6@users.noreply.github.com>
2025-10-02 08:46:54 -0400

    Update testDB.php
    Forgot to change DB IP

--- testDB.php
+++ testDB.php
@@ -1,5 +1,5 @@
 function testdb() {
-  $db = new mysqli('127.0.0.1','testUser','userPass','userdb');
+  $db = new mysqli('172.28.172.114','testUser','userPass','userdb');
   if ($db->connect_errno) { error_log('DB connect failed: '.$db->connect_error); return null; }
   $db->set_charset('utf8mb4');
   return $db;
