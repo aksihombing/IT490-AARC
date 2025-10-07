58fbaefdcecb96543de518c95283d6cc9e3d06f7
Rea <aksihombing05@gmail.com>
2025-10-06 23:23:13 -0400

    Created send.php and receive.php (scripts for broker's queues)

---
+++ rabbitMQ/receive.php
@@ -0,0 +1,70 @@
+#!/usr/bin/php
+<?php
+// CONSUMER - Receives messages from queue
+// https://www.rabbitmq.com/tutorials/tutorial-one-php -- Reference
+
+
+require_once('rabbitMQLib.inc'); // also references get_host_info.inc
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+
+$connection = new AMQPStreamConnection($BROKER_HOST, 5672, $USER, $PASSWORD);
+$channel = $connection->channel();
+
+$channel->queue_declare($queue, false, false, false, false);
+
+// debugging
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+$callback = function (AMQPMessage $msg) {
+  echo ' [x] Received ', $msg->getBody(), "\n"; 
+
+  $data = json_decode($msg->body, true);
+
+  $db = new mysqli('172.28.172.114','testUser','12345','testdb');
+
+  if ($db->connect_error){ // connection error
+    echo "DB connection failed: " . $db->connect_error . "\n";
+    return;
+  }
+
+  // REGISTER
+  if ($data['type'] === 'register'){
+    // initializes info that was SENT
+    $username = $data['username'];
+    $password = password_hash($data['password'], PASSWORD_BCRYPT);
+    $email = $data['email'];
+
+    $stmt = $db->prepare("INSERT INTO users username, password, email VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $password, $email);
+    $stmt->execute();
+    }
+  elseif  ($data['type'] === 'login'){
+    $username = $data['username'];
+    $password = $data['password'];
+
+    $stmt = $db->prepare("SELECT password FROM users WHERE username = ?"); // note that password isnt verified yet here
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->bind_result($hash);
+    $stmt->fetch();
+
+    if ($hash && password_verify($password,$hash)){
+        echo "LOGIN SUCCESSFUL for $username\n";
+    }
+    else {echo "INVALID LOGIN for $username\n";}
+  }
+  $db->close();
+};
+
+$channel->basic_consume($queue, '', false, true, false, false, $callback);
+
+try {
+    $channel->consume();
+} catch (\Throwable $exception) {
+    echo $exception->getMessage();
+}
+
+?>
