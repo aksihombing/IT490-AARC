45358783662b5f72a917ae2c3c5a1be337d8cb1b
Rea <aksihombing05@gmail.com>
2025-10-08 08:17:47 -0400

    Changed login and register's require_once paths

--- frontend/login.php
+++ frontend/login.php
@@ -1,7 +1,7 @@
 <?php
 // CHIZZY'S BASE CODE with edits from Rea for rabbitMQClient functions
 session_start();
-require_once('rabbitMQLib.inc');
+require_once(__DIR__.'../rabbitMQ/rabbitMQLib.inc');
 
 
 
--- frontend/register.php
+++ frontend/register.php
@@ -4,7 +4,7 @@
 // pulled from Chizzy's branch
 
 
-require_once('../rabbitMQ/rabbitMQLib.inc');
+require_once(__DIR__.'../rabbitMQ/rabbitMQLib.inc');
 
 
 
