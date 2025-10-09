d547ef10d4be5bb5f6a23b2b9ec42b76392eabc9
Chizi <157385976+Cae6@users.noreply.github.com>
2025-10-08 22:51:46 -0400

    Update login.php

--- WebServer/login.php
+++ WebServer/login.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-require_once('rabbitMQLib.inc');
+require_once __DIR__ . '/rabbitMQLib.inc';
+require_once __DIR__ . '/get_host_info.inc';   
+require_once __DIR__ . '/path.inc';
 
 
 
@@ -20,7 +22,7 @@ if ($username === '' || $password === '') {
 }
 
 try {
-  $client  = new rabbitMQClient("testRabbitMQ.ini","testServer");
+  $client = new rabbitMQClient(__DIR__ . "/testRabbitMQ.ini","testServer");
   $request  = ['type' => 'login', 'username' => $username, 'password' => $password];
   $response = $client->send_request($request);
 
