097858daac637d7f4779b0e363e31d96539d290c
a-looft <apl33@njit.edu>
2025-10-13 04:28:30 -0400

    changes to login/logout to with w queues and updated rmq files and test vm

--- frontend/logout.php
+++ frontend/logout.php
@@ -1,6 +1,26 @@
 <?php
 session_start();
-$_SESSION = [];
+require_once __DIR__ . '/rabbitMQ/rabbitMQLib.inc';
+require_once __DIR__ . '/rabbitMQ/get_host_info.inc';
+
+if (!isset($_SESSION['session_key'])) {
+    session_destroy();
+    header("Location: index.php");
+    exit;
+}
+
+$request = [
+    'type' => 'logout',
+    'session_key' => $_SESSION['session_key']
+];
+
+try {
+    $client = new rabbitMQClient(__DIR__ . "/rabbitMQ/host.ini", "AuthLogout");
+    $response = $client->send_request($request);
+} catch (Exception $e) {
+    // will still log out even if rmq fails i think
+}
+
 session_destroy();
-header('Location: index.php');
+header("Location: index.php");
 exit;
