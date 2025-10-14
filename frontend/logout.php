6491e436701fb8cb85e7e899cab8ddc733b7e854
aida looft <94566701+a-looft@users.noreply.github.com>
2025-10-13 23:33:14 -0400

    Update logout.php

--- frontend/logout.php
+++ frontend/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/rabbitMQ/rabbitMQLib.inc';
-require_once __DIR__ . '/rabbitMQ/get_host_info.inc';
+require_once __DIR__ . '/../rabbitMQ/rabbitMQLib.inc';
+require_once __DIR__ . '/../rabbitMQ/get_host_info.inc';
 
 if (!isset($_SESSION['session_key'])) {
     session_destroy();
@@ -14,11 +14,13 @@ $request = [
     'session_key' => $_SESSION['session_key']
 ];
 
+$response = null;
 try {
-    $client = new rabbitMQClient(__DIR__ . "/rabbitMQ/host.ini", "AuthLogout");
+    $client = new rabbitMQClient(__DIR__ . "/../rabbitMQ/host.ini", "AuthLogout");
     $response = $client->send_request($request);
 } catch (Exception $e) {
     // will still log out even if rmq fails i think
+    error_log("logout.php exception sending logout request: " . $e->getMessage());
 }
 
 session_destroy();
