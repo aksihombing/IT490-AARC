59aa68674fa8004dd8aa20e0beeb99f8889d4cfd
Angela V <76793603+vangela6780@users.noreply.github.com>
2025-10-22 18:44:47 -0400

    added club_members

--- clubs.sql
+++ clubs.sql
@@ -5,3 +5,11 @@ CREATE TABLE `clubs` (
   `description` TEXT NULL,
   PRIMARY KEY (`club_id`)
 );
+
+CREATE TABLE `club_members` (
+    'member_id' INT(11) NOT NULL AUTO_INCREMENT,
+    'club_id' INT(11) NULL,
+    'user_id' INT(11) NULL,
+    PRIMARY KEY (`member_id`),
+    MULTIPLE KEY (`club_id`)
+);
