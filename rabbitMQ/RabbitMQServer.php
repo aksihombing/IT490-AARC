4d459dbcf0b8d5edcccb305f3b558b30d8a86e16
a-looft <apl33@njit.edu>
2025-10-22 11:26:17 -0400

    added club processor to

--- rabbitMQ/RabbitMQServer.php
+++ rabbitMQ/RabbitMQServer.php
@@ -476,7 +476,7 @@ $iniPath = __DIR__ . "/host.ini";
 
 if ($which === 'all') { // to run all queues for DB and RMQ connection
     echo "Auth server starting for ALL queues...\n";
-    $sections = ['AuthRegister', 'AuthLogin', 'AuthValidate', 'AuthLogout'];
+    $sections = ['AuthRegister', 'AuthLogin', 'AuthValidate', 'AuthLogout', 'ClubProcessor'];
 
     foreach ($sections as $section) {
         $pid = pcntl_fork(); // process control fork; creats child process 
