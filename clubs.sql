6af20746591d3de87cc3dcfbd194d36d918d81c5
Angela <vangela6780@gmail.com>
2025-10-23 00:13:01 -0400

    added events_club exactly like in pic

--- clubs.sql
+++ clubs.sql
@@ -5,3 +5,20 @@ CREATE TABLE `clubs` (
   `description` TEXT NULL,
   PRIMARY KEY (`club_id`)
 );
+
+CREATE TABLE `club_members` (
+  `member_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `club_id` INT(11) NULL,
+  `user_id` INT(11) NULL,
+  PRIMARY KEY (`member_id`),
+    KEY (`club_id`)
+);
+
+CREATE TABLE `club_events` (
+  `event_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `club_id` INT(11) NULL,
+  `title` VARCHAR(100) NULL,
+  `event_date` DATE NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`event_id`),
+);
