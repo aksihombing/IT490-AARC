0ca81bcfcae1898b4a72b87c743c66cbd20cf35f
Rea Sihombing <aksihombing05@gmail.com>
2025-10-23 08:21:42 -0400

    remove brosw from main

--- frontend/main.inc.php
+++ frontend/main.inc.php
@@ -143,47 +143,6 @@ if (!isset($_SESSION['session_key'])):
       <?php endif; ?>
     </section>
 
-
-
-    <!-- BROWSING ----------------------- -->
-    <br> <br>
-
-    <section id="browse-section">
-      <h3>Browse Books</h3>
-      <!-- Browse results; SHOULD use the same structure as search.php and recent_books but isnt working ?? -->
-      <?php if (!empty($browseBooks)): ?>
-        <ul>
-          <?php foreach ($browseBooks as $book): ?>
-            <li>
-              <?php $olid = urlencode($book['olid']); ?>
-              <a href="index.php?content=book&olid=<?php echo htmlspecialchars($olid); ?>">
-
-              <br><br>
-                <?php if (!empty($book['cover_url'])): ?>
-                  <img src="<?php echo htmlspecialchars($book['cover_url']); ?>" alt="Cover" width="80">
-                <?php endif; ?>
-
-
-                <strong><?php echo htmlspecialchars($book['title']); ?></strong><br>
-                by <?php echo htmlspecialchars($book['author']); ?>
-              </a>
-            </li>
-          <?php endforeach; ?>
-        </ul>
-
-        <!-- Pagination links -->
-        <div class="pagination">
-          <?php if ($page > 1): ?>
-            <a href="index.php?page=<?php echo $page - 1; ?>">Previous</a>
-          <?php endif; ?>
-
-          <a href="index.php?page=<?php echo $page + 1; ?>">Next</a>
-        </div>
-      <?php else: ?>
-        <p>No books found for this category.</p>
-      <?php endif; ?> <!-- end if(!empty$browseBooks) -->
-    </section>
-
     <br>
     <br>
     <p><a id="logoutbutton" href="logout.php">Logout</a></p>
