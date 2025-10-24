51cf5bf78bd17a1b8530051f0f4b6b5c07a48b08
a-looft <apl33@njit.edu>
2025-10-23 22:55:49 -0400

    removed random s how did that get here even

--- frontend/clubs_functions.php
+++ frontend/clubs_functions.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo json_encode($res);
     } catch (Exception $e) {
         echo json_encode(['status'=>'fail','message'=>$e->getMessage()]);
-    }S
+    }
     exit;
 }
 ?>
