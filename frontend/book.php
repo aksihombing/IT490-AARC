ef4ff6ff2db6b1ae341a26b4a1f91cf196c3d13e
Chizi <157385976+Cae6@users.noreply.github.com>
2025-10-23 22:06:33 -0400

    added form names to revert

--- frontend/book.php
+++ frontend/book.php
@@ -35,8 +35,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_POST['action'] ?? '';
 
     if ($action === 'add_to_library'){
-      $Bclient = new rabbitMQClient(__DIR__ . '/../rabbitMQ/host.ini', 'LibraryPersonal');
-      $Bclient->send_request([
+      $addLibraryClient = new rabbitMQClient(__DIR__ . '/../rabbitMQ/host.ini', 'LibraryPersonal');
+      $addLibraryClient->send_request([
         'type'    => 'library.personal.add',
         'user_id' => $_SESSION['uid'],
         'works_id' => $olid,
@@ -58,8 +58,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $rating  = $_POST['rating']  ?? 0;
       $comment = $_POST['comment'] ?? '';
 
-      $Aclient = new rabbitMQClient(__DIR__ . '/../rabbitMQ/host.ini', 'CreateReviews');
-      $Aclient->send_request([
+      $createReviewClient = new rabbitMQClient(__DIR__ . '/../rabbitMQ/host.ini', 'CreateReviews');
+      $createReviewClient->send_request([
         'type'     => 'library.review.create',
         'user_id'  => $_SESSION['uid'],
         'works_id' => $olid,
@@ -80,8 +80,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 // -------------- DO BOOK DETAILS
 try {
-  $client = new rabbitMQClient(__DIR__ . '/../rabbitMQ/host.ini', 'LibraryDetails');
-  $response = $client->send_request([
+  $bookDetailsClient = new rabbitMQClient(__DIR__ . '/../rabbitMQ/host.ini', 'LibraryDetails');
+  $response = $bookDetailsClient->send_request([
     'type' => 'book_details',
     'olid' => $olid
   ]);
@@ -104,9 +104,9 @@ if (($response['status'] === 'success') && is_array($response)) {
 
 $reviews=[];
 try {
-  $Oclient = new rabbitMQClient(__DIR__ . '/../rabbitMQ/host.ini', 'ListReviews');
-  $resp = $Oclient->send_request([
-    'type'     => 'library.review.list',
+  $listReviewsClient = new rabbitMQClient(__DIR__ . '/../rabbitMQ/host.ini', 'ListReviews');
+  $resp = $listReviewsClient->send_request([
+    'type'     => 'library.reviews.list',
     'works_id' => $olid,
   ]);
   if ($resp['status'] === 'success') {
