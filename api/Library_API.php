978dbfe2a55d6cb572b10b840ebd010ca38fb7f6
Rea Sihombing <aksihombing05@gmail.com>
2025-11-04 22:14:57 -0500

    cleaned messy api comments

--- api/Library_API.php
+++ api/Library_API.php
@@ -69,28 +69,27 @@ function curl_get(string $url)
 // DATABASE CACHE VER --- !!
 function doBookSearch(array $req)
 {
-  $type = $req['searchType'] ?? 'title'; // to search by title
+  $type = $req['searchType'] ?? 'title';
   $query = strtolower(trim($req['query'] ?? ''));
   if ($query === '')
     return ['status' => 'fail', 'message' => 'missing query'];
 
-  // pagination and limit features
-  $limit = isset($req['limit']) && is_numeric($req['limit']) ? $req['limit'] : 10; // default limit is 10
-  $page = isset($req['page']) ? intval($req['page']) : 1; // default page starts at 1
-  // removed offset because its not really used
+  $limit = isset($req['limit']) && is_numeric($req['limit']) ? $req['limit'] : 10;
+  $page = isset($req['page']) ? intval($req['page']) : 1; 
+
 
 
   // CACHE CHECK ----------------------------------
   $mysqli = db();
 
-  echo "Checking cache for: type={$type}, query='{$query}', limit={$limit}, page={$page}\n"; //debugging
+  echo "Checking cache for: type={$type}, query='{$query}', limit={$limit}, page={$page}\n"; 
 
-  $check_cache = $mysqli->prepare("SELECT * FROM library_cache WHERE search_type=? AND query=? AND pageNum=? AND expires_at > NOW() LIMIT ?"); // might need to change limit ? idk
+  $check_cache = $mysqli->prepare("SELECT * FROM library_cache WHERE search_type=? AND query=? AND pageNum=? AND expires_at > NOW() LIMIT ?");
   $check_cache->bind_param("ssii", $type, $query, $page, $limit);
   $check_cache->execute();
   $cache_result = $check_cache->get_result();
 
-  if ($cache_result->num_rows > 0) { // if there is 1 or more results
+  if ($cache_result->num_rows > 0) { 
     echo "Cache HIT for {$type}={$query}\n";
     $cachedData = [];
 
@@ -99,7 +98,7 @@ function doBookSearch(array $req)
     }
 
     return ['status' => 'success', 'data' => $cachedData];
-    // RETURNS CACHED DATA !!!! AKA CACHE HIT !!!!!
+    // return cache HIT
   }
 
 
@@ -110,16 +109,15 @@ function doBookSearch(array $req)
 
   // for search.json!!!  ----------------------
   $encodedQuery = urlencode($query); // url encodes query when its actually getting sent to the API
-  $searchurl = "https://openlibrary.org/search.json?q={$encodedQuery}&limit={$limit}&page={$page}"; //base url for endpoint
+  $searchurl = "https://openlibrary.org/search.json?q={$encodedQuery}&limit={$limit}&page={$page}"; 
 
 
   $search_response = curl_get($searchurl);
-  $curl_data = json_decode($search_response, true); // true is for the associative arrays. if false, it returns the json objects into objects. make sure to decode the response from the api before upserting/ inserting it back into the db
+  $search_data = json_decode($search_response, true);
 
-  if (empty($curl_data['docs']))
+  if (empty($search_data['docs']))
     return ['status' => 'fail', 'message' => 'no results']; // no results found
 
-  // prepare insertToTable before reading the docs that are returned because reading each result from the query will be done in a FOREACH loop !!
   // $insertToTable will be repeatedly called from the loop
   $insertToTable = $mysqli->prepare("
     INSERT INTO library_cache (
@@ -144,14 +142,13 @@ function doBookSearch(array $req)
       cover_url=VALUES(cover_url),
       last_updated=CURRENT_TIMESTAMP
   ");
-  // ** need to look into if last_updated is updated correctly ?
+
 
   // https://github.com/internetarchive/openlibrary/blob/b4afa14b0981ae1785c26c71908af99b879fa975/openlibrary/plugins/worksearch/schemes/works.py#L119-L153
 
-  $searchbookresults = []; // empty array to get sent to the webserver, idk wehre it should actually go
-  // needs to be declared outside the loop to collect ALL book data
+  $searchbookresults = []; 
 
-  foreach ($curl_data['docs'] as $book) { // FOREACH BOOK START
+  foreach ($search_data['docs'] as $book) { // FOREACH BOOK START
     // reading each doc that was returned
     $olid = str_replace('/works/', '', $book['key'] ?? null); //string --> cover_edition_key was specific to the edition of a book and not the actual OLID value in /works/
     $title = $book['title'] ?? 'Unknown title'; //string
@@ -160,7 +157,6 @@ function doBookSearch(array $req)
     $publish_year = $book['first_publish_year'];
     $cover_url = !empty($book['cover_i'])
       ? "https://covers.openlibrary.org/b/id/" . $book['cover_i'] . "-L.jpg" : null; // ternary -> if cover_i is set, then it saves the link
-    // cover_i only saves the id of where the cover is, so we have to build the link manually
     // gets the -L (Large) version of the image
 
 
@@ -181,7 +177,7 @@ function doBookSearch(array $req)
 
       if (is_array($work_data['description'])) { // still getting a php warning idky
         $book_desc = $work_data['description']['value'];
-      } // some books have an array for description
+      } 
       else if (is_string($work_data['description'])) {
         $book_desc = $work_data['description'];
       } else {
@@ -209,7 +205,7 @@ function doBookSearch(array $req)
 
     if ($editions_json) {
       $editions_data = json_decode($editions_json, true);
-      $first_entry = $editions_data['entries'][0]; // gets the first entry result bc it will have ALL editions with different isbns listed. this is good enough for now, but it might be good to have a list of isbns.
+      $first_entry = $editions_data['entries'][0]; // gets the first entry result
 
       if (!empty($first_entry['isbn_13'][0])) {
         $isbn = $first_entry['isbn_13'][0];
@@ -238,7 +234,6 @@ function doBookSearch(array $req)
 
     echo "Saving to cache: type={$type}, query='{$query}'\n"; // debugging
 
-    // binding params for such a big table... nightmare fuel for anyone who craves efficiency
 
     $insertToTable->bind_param(
       "ssissssssidisssss",
@@ -259,7 +254,7 @@ function doBookSearch(array $req)
       $place_key, // json_encode(array)
       $time_key, // json_encode(array)
       $cover_url // string
-    ); // need to check if this was done correctly bc its just TOOO much
+    ); 
 
     $insertToTable->execute();
 
@@ -297,9 +292,7 @@ function getRecentBooks()
 {
   try {
     $mysqli = db();
-    $result = $mysqli->query("SELECT * FROM recentBooks ORDER BY publish_year DESC "); // switched to return all fields from recentBooks
-
-    // $result = $mysqli->query("SELECT title, author, publish_year, cover_url FROM recentBooks ORDER BY publish_year DESC "); // LIMIT 10 will return 10 results but the database only has 10 entries anyway
+    $result = $mysqli->query("SELECT * FROM recentBooks ORDER BY publish_year DESC ");
 
     $books = [];
     while ($row = $result->fetch_assoc()) {
@@ -316,7 +309,7 @@ function getRecentBooks()
   }
 }
 
-// (( scrapped Popular_books ))
+
 
 
 
@@ -329,7 +322,6 @@ function doBookDetails(array $req)
     return ['status' => 'fail', 'message' => 'missing olid for query'];
 
   $encodedOlid = urlencode($olid);
-  // this is based on if OLID actually exists
   $work_url = "https://openlibrary.org/works/{$encodedOlid}.json";
   $work_json = curl_get($work_url);
   $work_data = json_decode($work_json, true);
@@ -343,10 +335,10 @@ function doBookDetails(array $req)
 
   $title = $work_data['title'] ?? 'Unknown title';
 
-  if (isset($work_data['description'])) { // check if it actually exists
+  if (isset($work_data['description'])) {
     if (is_array($work_data['description'])) {
       $book_desc = $work_data['description']['value'];
-    } // some books have an array for description
+    }
     else if (is_string($work_data['description'])) {
       $book_desc = $work_data['description'];
     } else {
@@ -355,9 +347,9 @@ function doBookDetails(array $req)
   } else {
     $book_desc = "No book description available";
   }
+  
+  
   // need to encode the json because the database column is of JSON type
-
-
 
   $subjects = json_encode(array_slice($work_data['subjects'] ?? [], 0, 20)); // take the first 20 subjects max
   $person_key = json_encode(array_slice($work_data['subject_people'] ?? [], 0, 20));
@@ -365,12 +357,11 @@ function doBookDetails(array $req)
   $time_key = json_encode(array_slice($work_data['subject_times'] ?? [], 0, 20));
 
   // for search.json!!!  ----------------------
-  $searchbase = "https://openlibrary.org/search.json"; //base url for endpoint
   $encodedQuery = urlencode($title); // url encodes query when its actually getting sent to the API
-  $searchurl = "{$searchbase}?q={$encodedQuery}&limit=1";
+  $searchurl = "https://openlibrary.org/search.json?q={$encodedQuery}&limit=1";
 
   $search_response = curl_get($searchurl);
-  $curl_data = json_decode($search_response, true);
+  $search_data = json_decode($search_response, true);
 
 
   $author = 'Unknown author';
@@ -378,14 +369,14 @@ function doBookDetails(array $req)
   $publish_year = null;
   $cover_url = null;
 
-  if ($curl_data && isset($curl_data['docs'][0])) { // get first doc only
-    $doc = $curl_data['docs'][0]; // we only care about the first result
+  if ($search_data && isset($search_data['docs'][0])) { // get first doc only
+    $doc = $search_data['docs'][0]; 
     $subtitle = $doc['subtitle'] ?? null; //string
     $author = $doc['author_name'][0] ?? 'Unknown author'; //string
     $publish_year = $doc['first_publish_year'];
     $cover_url = !empty($doc['cover_i'])
       ? "https://covers.openlibrary.org/b/id/" . $doc['cover_i'] . "-L.jpg" : null; // ternary -> if cover_i is set, then it saves the link
-    // cover_i only saves the id of where the cover is, so we have to build the link manually
+
     // gets the -L (Large) version of the image
 
   }
@@ -398,7 +389,7 @@ function doBookDetails(array $req)
 
   if ($editions_json) {
     $editions_data = json_decode($editions_json, true);
-    $first_entry = $editions_data['entries'][0]; // gets the first entry result bc it will have ALL editions with different isbns listed. this is good enough for now, but it might be good to have a list of isbns.
+    $first_entry = $editions_data['entries'][0]; 
 
     if (!empty($first_entry['isbn_13'][0])) {
       $isbn = $first_entry['isbn_13'][0];
@@ -477,7 +468,7 @@ function doBookRecommend(array $req)
     $work_data = json_decode($work_json, true); // decode to read all data
 
 
-    // $allSubjects = array_map('strtolower', array_slice($work_data['subject'] ?? [], 0, 20));// get all subjects returned, limit to first 20 subjects and makes sure its all lowercase
+
     $allSubjects_raw = array_map('strtolower', array_slice($work_data['subjects'] ?? [], 0, 50)); // grab nearly all subjects
     // NOTE : /works/ uses PLURAL 'subjects' ....
     $filteredSubjects = []; // filtered subjects, 1-word
@@ -487,9 +478,9 @@ function doBookRecommend(array $req)
 
         // https://www.php.net/manual/en/function.preg-match.php
         // https://regexr.com/
-        // regex for php </3
         if (!preg_match('/^[a-z\-]+$/', $filtering_subject))
-            continue; // one-word subjects, allowing for hyphenated subjects also
+            continue; 
+        // one-word subjects, allowing for hyphenated subjects also
         // exclude anything with accented characters (beyond ascii char code 122)
 
         $filteredSubjects[] = $filtering_subject; // add good, single word subject to array
@@ -497,7 +488,7 @@ function doBookRecommend(array $req)
 
 
     $allSubjects = array_slice($filteredSubjects, 0, 20); // not sure if slicing is needed here
-    shuffle($allSubjects); // easier to select a random selection of two subjects to search with
+    shuffle($allSubjects);
     //print_r($allSubjects); // DEBUGGING
 
 
@@ -506,7 +497,9 @@ function doBookRecommend(array $req)
 
     //echo "Primary subject: {$subject1}\n"; // DEBUGGING
 
-    $subject2 = array_slice($allSubjects, 1, 20); //skip first array item (which is used as the primary search), use next 20 subjects as a fallback in case there isnt a match with any one of them
+    $subject2 = array_slice($allSubjects, 1, 20); 
+    //skip first array item (which is used as the primary search), use next 20 subjects as a fallback in case there isnt a match with any one of them
+
     //echo "Second subject options: \n"; // DEBUGGING
     //var_dump($subject2);
 
@@ -521,10 +514,7 @@ function doBookRecommend(array $req)
     $recommendedBook = null;
 
     // regex and trim filtering for subjects for the recommended books
-
-
-
-    // return recommended book's olid --> maybe return 
+ 
     foreach ($works as $oneBook) {
 
         $rec_work_id = $oneBook['key'] ?? ''; // key: XXX is there the works_id is
@@ -545,7 +535,7 @@ function doBookRecommend(array $req)
         foreach ($rec_subjects_raw as $r_subject) {
             $r_subject = trim($r_subject);
 
-            // regex for php </3 -- same as previous filtering
+            // same as previous filtering
             if (!preg_match('/^[a-z\-]+$/', $r_subject))
                 continue;
 
@@ -554,8 +544,8 @@ function doBookRecommend(array $req)
 
         //echo "filtered rec_subjects:";
         //print_r($rec_subjects); // DEBUGGING
-        //https://www.php.net/manual/en/function.array-intersect.php
 
+        //https://www.php.net/manual/en/function.array-intersect.php
 
         $matchedSubject = array_intersect($rec_subjects, $subject2);
         //echo "found matched subject: "; // DEBUGGING
