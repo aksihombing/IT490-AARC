42bdc79b1085356a613ad79283b836a31312c413
Rea Sihombing <aksihombing05@gmail.com>
2025-11-13 01:14:03 -0500

    Bundle Script - Fixed syntax with brackets + removed directory check.

--- shell/test_bundle.sh
+++ shell/test_bundle.sh
@@ -20,24 +20,19 @@ BUNDLE_NAME=$1
 FILE_PATH=$2
 TAR_NAME="${BUNDLE_NAME}.tar.gz"
 
-# need to check -d directory actually exists + verify success
-if [! -d "$FILE_PATH"]; then
-    echo "File path to '$FILE_PATH' not found.\n"
-fi
-
 # c-create z-zip f-output file -C change directory
 tar -czf "$TAR_NAME" -C "$FILE_PATH" .
-if [$? -ne 0]; then
-    echo "Failed to create tarball.\n"
+if [ $? -ne 0 ]; then
+    echo "Failed to create tarball."
     exit 1
 fi
 
 # $? = the exit status of the last command ran, exit status 0 usually means success; anything else means fail/error
 
 # tarball -> php script -> sends into RMQ -> received by Deploy VM
-php test_bundle.php "$TAR_NAME"
-if [$? -ne 0]; then
-    echo "Failed to send tarball to messaging queue.\n"
+php test_bundle.php "$TAR_NAME" # ERROR couldnt connect to rmq
+if [ $? -ne 0 ]; then
+    echo "Failed to send tarball to messaging queue."
     exit 1
 fi
 
@@ -45,11 +40,11 @@ fi
 echo "$BUNDLE_NAME : Successfully sent $SOURCE via RMQ"
 
 # need to SCP bundle into destination i think ? i dont know if this needs to be in the bundle script itself
-#sshpass -p "passw0rd" ssh aida@172.28.219.213 "scp rea@172.28.109.126:/home/rea/test.txt aida@172.28.219.213:/home/aida/"
+#sshpass -p "passw0rd" ssh aida@172.28.219.213 "scp rea-sihombing@172.28.109.126:/home/rea-sihombing/test.txt aida@172.28.219.213:/home/aida/"
 # scp (source) (destination)
-scp rea@172.28.109.126:/home/rea/test.txt aida@172.28.219.213:/home/aida/
-if [$? -ne 0]; then
-    echo "Failed to SCP file to destination.\n"
+scp rea-sihombing@172.28.109.126:/home/rea-sihombing/test.txt aida@172.28.219.213:/home/aida/
+if [ $? -ne 0 ]; then
+    echo "Failed to SCP file to destination."
     exit 1
 fi
-echo "Sent $BUNDLE_NAME via SCP\n"
+echo "Sent $BUNDLE_NAME via SCP"
