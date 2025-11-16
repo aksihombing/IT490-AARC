9d14ac1a7bbd75266ba2519abd28f209c7daa9a9
Angela V <76793603+vangela6780@users.noreply.github.com>
2025-11-15 19:31:33 -0500

    adding/drafting files for heartbeat for the hsb

--- hsb/hs.php
+++ hsb/hs.php
@@ -1,4 +1,10 @@
-// add the ip address of the primary vm & also the back up one 
+<?php>
 
-const primaryVM = "";
-const backVM = "";
+// this script is to go on the backup VM only 
+const primaryVM = "172.28.172.114";  // might need to be change bc it might be a differnt machine ip address? 
+const backVM = "172.28.172.115"; // i chose this bc im going to make it match with the backup vm 
+
+const heartBeatURL = "http://172.28.172.114/healthCheck.php";  
+
+</php>
+
