70bdd168145719bb3f462728e81f2f52ded5810f
Lucas Havriliak <lucashavriliak@outlook.com>
2025-11-18 00:25:32 -0500

    IT WORKS

---
+++ backend/rabbitMQ/daemon/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copy file into systemd
+sudo cp rabbitmqserver.service /etc/systemd/system
+
+# Reload daemon
+systemctl daemon-reload
+
+# Enable daemon
+sudo systemctl enable rabbitmqserver.service
+
+# Start daemon (I don't think we need to restart the whole computer. I may change that on the dmz machine later.)
+sudo systemctl start rabbitmqserver.service
