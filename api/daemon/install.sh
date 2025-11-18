2f7d602c0ff4af95df1a333ea2b5645faf03468a
Lucas Havriliak <lucashavriliak@outlook.com>
2025-11-18 00:29:56 -0500

    Fixed daemon installer bc restarting is stupid

--- api/daemon/install.sh
+++ api/daemon/install.sh
@@ -8,7 +8,8 @@ systemctl daemon-reload
 # Enable daemon
 sudo systemctl enable libraryapi.service
 
-# Reboot system
-echo libraryapi daemon installation successful! Rebooting in 10 seconds...
-sleep 10
-reboot
+# Start daemon
+sudo systemctl start libraryapi.service
+
+# User Notification
+echo Libraryapi daemon installation successful!
