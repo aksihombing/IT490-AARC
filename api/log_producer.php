85d6263b9488e0eff03a656d5ed17f664fda0811
aida looft <94566701+a-looft@users.noreply.github.com>
2025-11-19 18:00:54 -0500

    added distributive log files
    unsure if functioning fully yet

---
+++ api/log_producer.php
@@ -0,0 +1,27 @@
+<?
+require_once __DIR__ . '/rabbitMQLib.inc'; //will probably need to fix path like usual
+function log_event($vm, $type, $log){
+    $client = new rabbitMQClient(__DIR__ . "/host.ini", "logProducer"); // need .ini section for this
+
+    $log_map = [
+        'timestamp' => date("D M j G:i:s"), //timestamp formatted  Sat Mar 10 17:16:18 https://www.php.net/manual/en/function.date.php
+        'vm' => $vm, // frontend, backend, dmz
+        'type' => $type, //error, warning, etc
+        'log' => $log,
+    ];
+
+    $client->publish($log_map, "logs.fanout");
+
+    return true;
+}
+
+/*
+use this logic wherever we need to log errors? am i understanding this correctly
+
+if (!$variable/condition/etc){
+    log_event("vm_name","type","log")
+}
+
+*/
+
+?>
