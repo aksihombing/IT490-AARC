c1eb0fc424c23c4ad1051a75ba514440c39458f7
Lucas Havriliak <lucashavriliak@outlook.com>
2025-11-19 22:30:23 -0500

    added new daemon to installer. Hopefully no errors :D

--- daemon/install.sh
+++ daemon/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Copy file into systemd
 sudo cp rabbitmqserver.service /etc/systemd/system
+sudo cp loglistener.service /etc/systemd/system
 
 # Reload daemon
 systemctl daemon-reload
 
 # Enable daemon
 sudo systemctl enable rabbitmqserver.service
-
+sudo systemctl enable loglistener.service
 # Start daemon (I don't think we need to restart the whole computer. I may change that on the dmz machine later.)
 sudo systemctl start rabbitmqserver.service
+sudo systemctl start loglistener.service
