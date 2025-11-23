b61d97443d40e816a9eac1da254f9c1e4c9a045f
Angela V <76793603+vangela6780@users.noreply.github.com>
2025-11-22 21:30:16 -0500

    fixing the syntax for line 6 & 12

--- hsb/db.sh
+++ hsb/db.sh
@@ -1,12 +1,15 @@
-##!/bin/bash
+#!/bin/bash
 LOG_FILE="/var/log/hsb_db_failover.log"
-echo "starting the db takeover process on the backup vm" >> "$LOG_FILE"
-echo "stopping the db service " >> "$LOG_FILE"
-sudo systemctl stop mysql-server
 
-echo "forcing primary role on my sql" >> "$LOG_FILE"
+echo "starting db takeover process on the backup vm" >> "$LOG_FILE"
+echo "stopping sql server service to break replication link" >> "$LOG_FILE"
+sudo systemctl stop mssql-server
+
+echo "forcing primary role on sql server" >> "$LOG_FILE"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'pccc' -Q "ALTER DATABASE [userdb] SET PRIMARY" >> "$LOG_FILE" 2>&1
-# might need to change the db and user credentials
 
-echo "restarting the mysql service as new primary" >> "$LOG_FILE"
-sudo systemctl restart mysql-server
+echo "restarting sql server service as new primary" >> "$LOG_FILE"
+sudo systemctl restart mssql-server
+
+sleep 5
+exit 0
