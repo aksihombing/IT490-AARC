9af4cea3b19d960d4511293270f40114916f3721
Angela V <76793603+vangela6780@users.noreply.github.com>
2025-11-22 21:27:42 -0500

    adding command to restart mysql server

--- hsb/db.sh
+++ hsb/db.sh
@@ -2,10 +2,11 @@
 LOG_FILE="/var/log/hsb_db_failover.log"
 echo "starting the db takeover process on the backup vm" >> "$LOG_FILE"
 echo "stopping the db service " >> "$LOG_FILE"
-sudo systemctl stop mysql
+sudo systemctl stop mysql-server
 
 echo "forcing primary role on my sql" >> "$LOG_FILE"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'pccc' -Q "ALTER DATABASE [userdb] SET PRIMARY" >> "$LOG_FILE" 2>&1
 # might need to change the db and user credentials
 
 echo "restarting the mysql service as new primary" >> "$LOG_FILE"
+sudo systemctl restart mysql-server
