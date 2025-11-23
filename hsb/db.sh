7ba41e72a69b0dd4b5f615fd210067fda9b42fde
Angela V <76793603+vangela6780@users.noreply.github.com>
2025-11-22 20:19:44 -0500

    adding the sudo command to stop the mysql stuff

--- hsb/db.sh
+++ hsb/db.sh
@@ -2,3 +2,4 @@
 LOG_FILE="/var/log/hsb_db_failover.log"
 echo "starting the db takeover process on the backup vm" >> "$LOG_FILE"
 echo "stopping the db service " >> "$LOG_FILE"
+sudo systemctl stop mysql
