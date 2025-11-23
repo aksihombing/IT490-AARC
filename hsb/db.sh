ff5894221a41d8ceaacb83aeb14f5c93ba35c447
Angela V <76793603+vangela6780@users.noreply.github.com>
2025-11-22 20:34:52 -0500

    adding a msg for forcing primary

--- hsb/db.sh
+++ hsb/db.sh
@@ -3,3 +3,5 @@ LOG_FILE="/var/log/hsb_db_failover.log"
 echo "starting the db takeover process on the backup vm" >> "$LOG_FILE"
 echo "stopping the db service " >> "$LOG_FILE"
 sudo systemctl stop mysql
+
+echo "forcing primary role on my sql" >> "$LOG_FILE"
