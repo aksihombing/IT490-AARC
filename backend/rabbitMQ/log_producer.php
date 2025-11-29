b9b633e9321804c196ae9786048ce4dd617f3ead
a-looft <apl33@njit.edu>
2025-11-29 13:27:03 -0500

    changes to log producer

--- backend/rabbitMQ/log_producer.php
+++ backend/rabbitMQ/log_producer.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/rabbitMQLib.inc'; //will probably need to fix path like usual
 function log_event($vm, $type, $log){
-    $client = new rabbitMQClient(__DIR__ . "/host.ini", "logListener"); 
+    $client = new rabbitMQClient(__DIR__ . "/host.ini", "logProducer"); 
 
     $log_map = [
         'timestamp' => date("D M j G:i:s"), //timestamp formatted  Sat Mar 10 17:16:18 https://www.php.net/manual/en/function.date.php
