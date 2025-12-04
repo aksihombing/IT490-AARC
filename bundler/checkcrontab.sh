1da473752cf5f02558d572c5c0d7cc047c1bc1ee
Rea Sihombing <aksihombing05@gmail.com>
2025-12-03 23:04:59 -0500

    updated cron job again to silence 'no crontabs'

--- bundler/checkcrontab.sh
+++ bundler/checkcrontab.sh
@@ -5,7 +5,7 @@ if [ $? -eq 0 ]; then echo 'Cron job updateRecentBooks.php already exists.';
     
 else
     # if it doesnt exist, make sure to re-make the cronjob
-    crontab -l; echo '0 10 * * * /usr/bin/php /home/aida/cron/updateRecentBooks.php >> /home/aida/cron/recentBooks.log.txt 2>&1' | crontab -
+    crontab -l 2>/dev/null; echo '0 10 * * * /usr/bin/php /home/aida/cron/updateRecentBooks.php >> /home/aida/cron/recentBooks.log.txt 2>&1' | crontab -
     
     if [ $? -ne 0 ]; then echo 'Failed to install cronjob.'; exit 1; fi
     
