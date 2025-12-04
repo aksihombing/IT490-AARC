fc2fd77c7a5bba0b930b4d076f31225a215f7d83
Rea Sihombing <aksihombing05@gmail.com>
2025-12-03 22:59:19 -0500

    Fixed logic for crontab

--- bundler/checkcrontab.sh
+++ bundler/checkcrontab.sh
@@ -1,9 +1,14 @@
 crontab -l | grep 'updateRecentBooks.php'
 
 # if the job exists, we dont need to update its cron
-if [ $? -eq 1 ]; then echo 'Cron job updateRecenBooks.php already exists.'; exit 1; fi
+if [ $? -eq 0 ]; then echo 'Cron job updateRecentBooks.php already exists.';
+    
+else
+    # if it doesnt exist, make sure to re-make the cronjob
+    crontab -l; echo '0 10 * * * /usr/bin/php /home/aida/cron/updateRecentBooks.php >> /home/aida/cron/recentBooks.log.txt 2>&1' | crontab -
+    
+    if [ $? -ne 0 ]; then echo 'Failed to install cronjob.'; exit 1; fi
+    
+    echo 'Cron job updateRecenBooks.php added.'
+fi
 
-# if it doesnt exist, make sure to re-make the cronjob
-crontab -l; echo '0 10 * * * /usr/bin/php /home/aida/cron/updateRecentBooks.php >> /home/aida/cron/recentBooks.log.txt 2>&1' | crontab -
-
-if [ $? -ne 0 ]; then echo 'Failed to install cronjob.'; exit 1; fi
