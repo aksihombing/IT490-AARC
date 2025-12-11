65ae94d4835b59712c6a7b94713b699cd6abcb86
Rea Sihombing <aksihombing05@gmail.com>
2025-12-11 01:45:42 -0500

    Forgot to also update the install script AGHHH

--- backend/rabbitMQ/daemon/install.sh
+++ backend/rabbitMQ/daemon/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Copy .service file into /etc/systemd/system
-sudo cp libraryapi.service /etc/systemd/system
+sudo cp rabbitmqserver.service /etc/systemd/system
 sudo cp loglistener.service /etc/systemd/system
 # Reload daemon
 systemctl daemon-reload
 
 # Enable daemon
-sudo systemctl enable libraryapi.service
+sudo systemctl enable rabbitmqserver.service
 sudo systemctl enable loglistener.service
 
 # Start daemon
-sudo systemctl start libraryapi.service
+sudo systemctl start rabbitmqserver.service
 sudo systemctl start loglistener.service
 # User Notification
-echo Libraryapi daemon installation successful!
+echo Rabbitmqserver daemon installation successful!
