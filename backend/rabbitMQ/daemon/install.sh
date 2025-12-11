1ebf1f4bc404ce66056af4644bd8f5050dbcec59
Rea Sihombing <aksihombing05@gmail.com>
2025-12-11 01:41:58 -0500

    backend rabbitMQ daemon wasnt updated ?

--- backend/rabbitMQ/daemon/install.sh
+++ backend/rabbitMQ/daemon/install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# Copy file into systemd
-sudo cp rabbitmqserver.service /etc/systemd/system
-
+# Copy .service file into /etc/systemd/system
+sudo cp libraryapi.service /etc/systemd/system
+sudo cp loglistener.service /etc/systemd/system
 # Reload daemon
 systemctl daemon-reload
 
 # Enable daemon
-sudo systemctl enable rabbitmqserver.service
+sudo systemctl enable libraryapi.service
+sudo systemctl enable loglistener.service
 
-# Start daemon (I don't think we need to restart the whole computer. I may change that on the dmz machine later.)
-sudo systemctl start rabbitmqserver.service
+# Start daemon
+sudo systemctl start libraryapi.service
+sudo systemctl start loglistener.service
+# User Notification
+echo Libraryapi daemon installation successful!
