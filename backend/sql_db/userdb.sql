77fbd755f2326976c64481ca1f624563d0ad2146
a-looft <apl33@njit.edu>
2025-12-18 16:28:15 -0500

    updated event attendees table

--- backend/sql_db/userdb.sql
+++ backend/sql_db/userdb.sql
@@ -61,16 +61,11 @@ CREATE TABLE `events` (
 );
 
 CREATE TABLE `EventAttendees` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `eventID` INT(11) NOT NULL,
   `userID` INT(11) NOT NULL,
-  `rsvpStatus` VARCHAR(20) NOT NULL DEFAULT 'Going',
-  `attended` BOOLEAN DEFAULT FALSE,
+  `rsvpStatus` ENUM('going','not_going') NOT NULL DEFAULT 'going',
   `rsvpDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `eventUserUnique` (`eventID`, `userID`), 
-  CONSTRAINT `fkEventAttendeeEventID` FOREIGN KEY (`eventID`) REFERENCES `events` (`eventID`) ON DELETE CASCADE,
-  CONSTRAINT `fkEventAttendeeUserID` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  PRIMARY KEY (`eventID`,`userID`)
 );
 
 CREATE TABLE user_library (
